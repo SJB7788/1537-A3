5fc163bbc6d27b53082fe25592745c291e5e88d7
SeungJae <sbj7788@gmail.com>
2023-04-06 14:47:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { default: mongoose } = require('mongoose');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(cors());
 
